95f00c68a2705c73e164769d446a8f0a917ddc3d
ctooley21 <unconfigured@null.spigotmc.org>
2018-03-04 10:00:01 -0500

    Finish NutritionUtil

--- app/src/main/java/com/google/sample/snapsey/NutritionUtil.java
+++ app/src/main/java/com/google/sample/snapsey/NutritionUtil.java
@@ -1,27 +1,20 @@
 package com.google.sample.snapsey;
 
-import android.preference.PreferenceActivity;
 import android.util.Log;
 
-import com.google.api.client.http.HttpResponse;
-
-import org.apache.http.Header;
 import org.apache.http.NameValuePair;
-import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.entity.UrlEncodedFormEntity;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.impl.client.DefaultHttpClient;
 import org.apache.http.message.BasicNameValuePair;
-import org.apache.http.util.EntityUtils;
-import org.json.JSONArray;
 import org.json.JSONObject;
 import org.json.JSONTokener;
 
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class NutritionUtil
@@ -29,38 +22,106 @@ public class NutritionUtil
 
     private static String appId;
     private static String appKey;
+    private static List<String> glutenItems;
 
     public static void init()
     {
         appId = "1178665d";
         appKey = "0f21996549fcf98881c28c9aaa08fc2f";
+        glutenItems = Arrays.asList("wheat", "barley", "rye", "oat");
     }
 
     public static void postData(String item) {
+        Log.d("GI", calculateGILevels(item)+"");
+        Log.d("GI", calculateGlucose(item)+"");
+    }
+
+    public static int calculateGILevels(String name)
+    {
         HttpClient httpclient = new DefaultHttpClient();
         HttpPost httppost = new HttpPost("https://api.nutritionix.com/v1_1/search");
 
         try {
-            List<NameValuePair> nameValuePairs = new ArrayList<>(2);
+            List<NameValuePair> nameValuePairs = new ArrayList<>();
             nameValuePairs.add(new BasicNameValuePair("appId", appId));
             nameValuePairs.add(new BasicNameValuePair("appKey", appKey));
-            nameValuePairs.add(new BasicNameValuePair("query", item));
+            nameValuePairs.add(new BasicNameValuePair("query", name));
             nameValuePairs.add(new BasicNameValuePair("limit", Integer.toString(1)));
-            nameValuePairs.add(new BasicNameValuePair("fields[]", "nf_calories"));
             nameValuePairs.add(new BasicNameValuePair("fields[]", "nf_dietary_fiber"));
+            nameValuePairs.add(new BasicNameValuePair("fields[]", "nf_serving_weight_grams"));
             nameValuePairs.add(new BasicNameValuePair("fields[]", "nf_total_carbohydrate"));
-            nameValuePairs.add(new BasicNameValuePair("fields[]", "nf_ingredient_statement"));
             httppost.setEntity(new UrlEncodedFormEntity(nameValuePairs));
 
             org.apache.http.HttpResponse response = httpclient.execute(httppost);
 
-            String responseBody = EntityUtils.toString(response.getEntity());
+            BufferedReader reader = new BufferedReader(new InputStreamReader(response.getEntity().getContent(), "UTF-8"));
+            StringBuilder builder = new StringBuilder();
+            for (String line; (line = reader.readLine()) != null;) {
+                builder.append(line).append("\n");
+            }
+            JSONTokener tokener = new JSONTokener(builder.toString());
+            JSONObject finalResult = new JSONObject(tokener);
 
-            Log.d("test", responseBody);
+            double fiber = finalResult.getJSONArray("hits").getJSONObject(0).getJSONObject("fields").getInt("nf_dietary_fiber");
+            double grams = finalResult.getJSONArray("hits").getJSONObject(0).getJSONObject("fields").getInt("nf_serving_weight_grams");
+            double carbs = finalResult.getJSONArray("hits").getJSONObject(0).getJSONObject("fields").getInt("nf_total_carbohydrate");
+            double level = ((carbs - fiber) / grams) * 100.0;
+
+            if(level > 100) level = 100;
+            if(level < 0) level = 0;
+
+            return (int) level;
 
         } catch (Exception e)
         {
-            e.printStackTrace();
+            return 0;
         }
     }
+
+    public static int calculateGlucose(String name)
+    {
+        HttpClient httpclient = new DefaultHttpClient();
+        HttpPost httppost = new HttpPost("https://api.nutritionix.com/v1_1/search");
+
+        try {
+            List<NameValuePair> nameValuePairs = new ArrayList<>();
+            nameValuePairs.add(new BasicNameValuePair("appId", appId));
+            nameValuePairs.add(new BasicNameValuePair("appKey", appKey));
+            nameValuePairs.add(new BasicNameValuePair("query", name));
+            nameValuePairs.add(new BasicNameValuePair("limit", Integer.toString(1)));
+            nameValuePairs.add(new BasicNameValuePair("fields[]", "nf_dietary_fiber"));
+            nameValuePairs.add(new BasicNameValuePair("fields[]", "nf_total_carbohydrate"));
+            httppost.setEntity(new UrlEncodedFormEntity(nameValuePairs));
+
+            org.apache.http.HttpResponse response = httpclient.execute(httppost);
+
+            BufferedReader reader = new BufferedReader(new InputStreamReader(response.getEntity().getContent(), "UTF-8"));
+            StringBuilder builder = new StringBuilder();
+            for (String line; (line = reader.readLine()) != null;)
+            {
+                builder.append(line).append("\n");
+            }
+            JSONTokener tokener = new JSONTokener(builder.toString());
+            JSONObject finalResult = new JSONObject(tokener);
+
+            String nutt = finalResult.getJSONArray("hits").getJSONObject(0).getJSONObject("fields").getString("nf_ingredient_statement");
+
+            int level = 0;
+
+            for(String s : glutenItems)
+            {
+                if(nutt.toLowerCase().contains(s))
+                {
+                    level += 25;
+                }
+            }
+
+            return level;
+
+        } catch (Exception e)
+        {
+            return 0;
+        }
+    }
+
 }
